263b0921dee8847a580cf22371edd8a4665fbc7a
jaimrooc <jaimrooc@gmail.com>
2017-12-01 01:05:06 -0500

    Test config

--- multi_notes_backend/config.js
+++ multi_notes_backend/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     'secret': 'claveSecreta',
     'database': 'mongodb://localhost/multinotes_bd',
-    'port': 1000
+    // 'port': 1000 // Windows OS
+    'port': 10443 // Linux OS
 };
